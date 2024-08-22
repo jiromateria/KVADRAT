92010558afa3bb1459c91e70e9add5a10456b98f
jiromateria <kolyasorokin2018@mail.ru>
2024-08-23 01:01:29 +0700

    Add files via upload

---
+++ кв ур3/a.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <TXLib.h>
+#include <math.h>
+#include <ctype.h>
+
+
+void PrintMessage(const char* Message);
+
+int main()
+{
+    PrintMessage("Ââåäèòå àqqqqqqq\ndfsdsdsfs");
+
+    return 0;
+}
+
+
+void PrintMessage(const char* Message)
+{
+    printf("%s",Message);
+}
